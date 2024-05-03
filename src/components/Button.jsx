8cf2206aa903432aa5bed96f183bc3edcdd800fb
Mohinuddin Razi <mohinuddin6razi@gmail.com>
2024-05-03 16:31:41 +0530

    resume updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button() {
       <br></br>
       <div className="d-grid gap-3 col-4 mx-auto">
         <a
-          href="https://drive.google.com/file/d/1U8FbkjDUbkNenTki9HrsW_psgNWo4Wew/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1Do8BDAbMrVLZN1dEIhTTOzXfrojoVU_Z/view?usp=drive_link"
           target="_blank"
           className="btn btn-danger"
           id={styles.btn1}
